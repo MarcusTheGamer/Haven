db3001aad193564c69d610e25c744f4816e83f6a
Marcus Liam Nielsen <splitscreen@cyberbyte.zip>
2026-09-14 21:32:15 +0200

    Updated Stuff

---
+++ Models/Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haven.Models
+{
+    class Device
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool IsOnline { get; set; }
+    }
+}
